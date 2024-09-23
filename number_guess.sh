ffc9e9c890e95b1694ee4f8892ddbc5bb8883517
Matei Ciurica <matei@neogen.biz>
2024-09-24 01:22:19 +0300

    feat: Add guessing game logic to number_guess.sh

--- number_guess.sh
+++ number_guess.sh
@@ -15,3 +15,11 @@ else
   IFS="|" read USER_ID GAMES_PLAYED BEST_GAME <<< "$USER_QUERY"
   echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
+
+echo "Guess the secret number between 1 and 1000:"
+NUMBER_OF_GUESSES=0
+RANDOM_NUMBER=$(( RANDOM % 1000 + 1 ))
+
+while true; do
+  read GUESS
+done
