b200b3f0e65ac19be5b7c0665afc04af19899fd8
Matei Ciurica <matei@neogen.biz>
2024-09-24 01:20:46 +0300

    feat: add user input and database query

--- number_guess.sh
+++ number_guess.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
+echo "Enter your username:"
+read USERNAME
+
+USER_QUERY=$($PSQL "SELECT user_id, games_played, best_game FROM users WHERE username='$USERNAME'")
